6afd5adbefab5be5648962a160326a43a63802c6
tishoyanchev <tihomir.yanchev-EE@infineon.com>
2023-09-20 08:28:45 +0200

    new test

---
+++ src/components/feedbackWidget/feedbackWdget.tsx
@@ -0,0 +1,19 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'ifx-feedbackWidget',
+  styleUrl: 'feedbackWidget.css',
+  shadow: true,
+})
+
+export class FeedbackWidget {
+
+ 
+  render() {
+    return (
+      <div>
+        test
+      </div>
+    );
+  }
+}
